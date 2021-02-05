0f8ad16cc0cb239a1bb67c14c8a67ab10a807ade
vavomiskam <vavomiskam@yandex.ru>
2021-02-05 22:10:06 +0300

    add LagrangeInterpolatorTest

---
+++ src/test/java/users/homework/codetotest/LagrangeInterpolatorTest.java
@@ -0,0 +1,12 @@
+package users.homework.codetotest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LagrangeInterpolatorTest {
+
+    @Test
+    public void getY() {
+    }
+}
